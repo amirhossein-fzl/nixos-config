51c8261379413cae00e119f032593d1739feb0cc
Amirhossein Fazli <amirhossein95b@gmail.com>
2025-09-18 20:51:45 +0330

    Refactor boot config.

--- modules/boot.nix
+++ modules/boot.nix
@@ -13,14 +13,14 @@
         device = "nodev";
         enableCryptodisk = true;
         extraEntries = ''
-          menuentry 'Arch Linux (on /dev/nvme0n1p5)' --class arch --class gnu-linux --class gnu --class os $menuentry_id_option 'gnulinux-2b3b0e2f-01b1-4345-8475-688555f92e4e' {
+          menuentry 'Arch Linux (on /dev/nvme0n1p5)' --class arch --class gnu-linux --class gnu --class os {
             insmod part_gpt
             insmod fat
             search --no-floppy --fs-uuid --set=root 1A1D-D728
-            linux /archlinux/vmlinuz-linux rocot=UUID=2b3b0e2f-01b1-4345-8475-688555f92e4e numlock=1 loglevel=4 lsm=landlock,yama,bpf
-              initrd /archlinux/initramfs-linux.img
+            linux /archlinux/vmlinuz-linux root=UUID=2b3b0e2f-01b1-4345-8475-688555f92e4e numlock=1 loglevel=4 lsm=landlock,yama,bpf
+            initrd /archlinux/initramfs-linux.img
           }
-          menuentry 'Windows Boot Manager (on /dev/nvme0n1p1)' --class windows --class os $menuentry_id_option 'efi-1A1D-D728' {
+          menuentry 'Windows Boot Manager (on /dev/nvme0n1p1)' --class windows --class os {
             insmod part_gpt
             insmod fat
             search --no-floppy --fs-uuid --set=root 1A1D-D728
