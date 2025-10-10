0994dd1365c4b97259545121f2c0c6548ffe6013
Amirhossein Fazli <amirhossein95b@gmail.com>
2025-10-10 10:30:17 +0330

    Add kde connect.

--- modules/networking.nix
+++ modules/networking.nix
@@ -3,13 +3,17 @@
 {
   networking = {
     hostName = "nixos";
-    firewall = {
+    firewall = rec {
       allowedTCPPorts = [ 1723 ]; # PPTP control port
       # PPTP uses GRE protocol (IP protocol 47)
       extraCommands = ''
         iptables -A INPUT -p gre -j ACCEPT
         iptables -A OUTPUT -p gre -j ACCEPT
       '';
+
+      # Kde connect
+      allowedTCPPortRanges = [ { from = 1714; to = 1764; } ];
+      allowedUDPPortRanges = allowedTCPPortRanges;
     };
     networkmanager = {
       enable = true;
--- users/amirhossein/services/default.nix
+++ users/amirhossein/services/default.nix
@@ -1,5 +1,6 @@
 {
   imports = [
+    ./kdeconnect.nix
     ./ollam.nix
   ];
 }
